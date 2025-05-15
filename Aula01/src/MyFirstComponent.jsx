6b4b6f223d6a45a62c3aed33d84448d5a13edb30
Mateus Lucena <150374285+Mathewxs21@users.noreply.github.com>
2025-05-15 17:47:24 -0300

    Add files via upload

---
+++ Aula01/src/MyFirstComponent.jsx
@@ -0,0 +1,17 @@
+function  MyFirstComponent () {
+
+    const name = 'Ariana'
+
+    return (
+        <>
+        <div>
+            <h1>I got one less problem without ya</h1>
+        </div>
+        <div>
+            <p>Its {name}</p>
+        </div>
+        </>
+    )
+}
+
+export default MyFirstComponent 
