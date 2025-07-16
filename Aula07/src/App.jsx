891bb989852935b6198772bd3b2875a10affca39
Mateus Lucena Pereira <20221101110017@ifrn.local>
2025-07-16 13:32:40 -0300

    UseEffect

---
+++ Aula07/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import CountClicks from './componentes/CountClicks'
+import EffectAPI from './componentes/EffectAPI'
+import RepoJson from './componentes/RepoJson'
+
+
+function App() {
+
+  return (
+    <>
+      
+     <EffectAPI />
+      
+      {/* <CountClicks/> */}
+      
+    </>
+  )
+}
+
+export default App
